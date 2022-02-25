e3088b1138cc4f8ec140d767f895e1320a64b7a6
Kiruha01 <liss-2001g@mail.ru>
2022-02-25 11:22:57 +0000

    Create application

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,28 @@
 from flask import Flask
+from models import db
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+import config
 
 
-if __name__ == '__main__':
-    app.run()
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_object(config.Config)
+    db.init_app(app)
+
+    # app.register_blueprint(locations, url_prefix='/api/locations/')
+
+    # @app.cli.command()
+    # def recreate_db():
+    #     # db.drop_all()
+    #     # db.create_all()
+    #     pass
+    #
+    # @app.cli.command()
+    # def create_data():
+    #     pass
+
+    # with app.app_context():
+    #     db.create_all()  # Create sql tables for our data models
+
+    return app
